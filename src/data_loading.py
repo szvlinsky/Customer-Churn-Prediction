d63fcee295cf39a72cf8c4a0277ca77a27f5eea4
wiciu <w.szylinski@op.pl>
2025-07-22 22:34:38 +0200

    feat: add data loading, pyproject setup and ignore rules

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,8 +1,11 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 
-def load_data(data_dir: str = "data/raw") -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    data_path = Path(data_dir)
+def load_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    project_root = Path(__file__).resolve().parents[1]
+    data_path = project_root / "data" / "raw"
+
+    print("Data path:", data_path)
 
     transactions_df = pd.read_csv(data_path / "transactions_train.csv")
     customers_df = pd.read_csv(data_path / "customers.csv")
@@ -10,10 +13,9 @@ def load_data(data_dir: str = "data/raw") -> tuple[pd.DataFrame, pd.DataFrame, p
 
     return transactions_df, customers_df, articles_df
 
-def merge_data(transactions_df: pd.DataFrame, customers_df: pd.DataFrame, articles_df: pd.DataFrame) -> pd.DataFrame:
+def merge_data(transactions_df: pd.DataFrame, articles_df: pd.DataFrame) -> pd.DataFrame:
     merged_df = (
         transactions_df
-        .merge(customers_df, on="customer_id", how="left")
         .merge(articles_df, on="article_id", how="left")
     )
     return merged_df
