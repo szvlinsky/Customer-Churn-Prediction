0ab894090de0ab6d23df0d3be2fd4bc36bb7bfb0
wiciu <w.szylinski@op.pl>
2025-08-28 12:31:25 +0200

    feat: add correct pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,10 +1,22 @@
 import sys
 from pathlib import Path
-
-from src.data_loading import load_data, merge_data
-
-
 sys.path.append(str(Path().resolve().parent)) # Katalog główny repo do sys.path
 
-transactions_df, customers_df, articles_df = load_data()
-df = merge_data(transactions_df, articles_df)
+from bayes_search import load_split_data, load_xgb_bayes_config, tune_xgb_hyperparams
+from model import xgb_train
+
+# 1. Ładowanie danych i ustawień
+X_train, y_train, X_val, y_val, X_test, y_test = load_split_data()
+cfg, space = load_xgb_bayes_config()
+print("Konfiguracja:", cfg)
+print("Przestrzeń hiperparametrów:")
+for param in space:
+    print(param)
+
+# 2. Wyszukanie optymalnych hiperparametrów
+best_params = tune_xgb_hyperparams(X_val, y_val, cfg, space)
+print("Najlepsze hiperparametry:", best_params)
+
+# 3. Trenowanie i test finalnego modelu
+booster = xgb_train(X_train, y_train, X_test, y_test, best_params, 
+                    num_boost_round=500, early_stopping_rounds=100)
